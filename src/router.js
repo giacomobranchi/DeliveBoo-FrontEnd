2466e456438708056e14e6295c21363cd9d29768
lorenzo-neri <lorenzo21neri@gmail.com>
2023-12-08 16:18:11 +0100

    add restaurants and single restaurants views

--- src/router.js
+++ src/router.js
@@ -1,12 +1,25 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
 
 
+import RestaurantsView from './views/RestaurantsView.vue';
+import SingleRestaurantView from './views/SingleRestaurantView.vue';
 
 const router = createRouter({
 
     history: createWebHashHistory(),
 
-    routes: []
+    routes: [
+        {
+            path: '/restaurants',
+            name: 'restaurants',
+            component: RestaurantsView
+        },
+        {
+            path: '/restaurant/:slug',
+            name: 'restaurant',
+            component: SingleRestaurantView
+        },
+    ]
 });
 
 export { router }
