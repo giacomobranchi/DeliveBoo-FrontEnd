42a9e543dc0d4da26c1c7b12f743ce8f0e5484f8
FabioVasi <vasifabio96@gmail.com>
2023-12-11 15:59:38 +0100

    add img bg and scroll bar restaurant type

--- src/views/RestaurantsView.vue
+++ src/views/RestaurantsView.vue
@@ -20,51 +20,59 @@ export default {
 
 <template>
     <main>
-
-        <div class="container">
-            <div class="row py-5">
-                <div class="col-3">
-                    <h3>Filtra per tipologia</h3>
-                    <div class="text-decoration-none m-1" v-for="type in this.state.types">
-                        <router-link :to="{ name: 'singleType', params: { slug: type.slug } }"
-                            class="text-decoration-none btn btn-primary">
-                            {{ type.name }}
-                        </router-link>
+        <section>
+            
+            <div class="container text-white">
+                <div class="row py-5">
+                    <div class="col-3">
+                        <h3 class="text-uppercase">Filtra per tipologia</h3>
+                        <div class="type_list overflow_hidden">
+                            <div class="text-decoration-none m-1" v-for="type in this.state.types">
+                                <router-link :to="{ name: 'singleType', params: { slug: type.slug } }"
+                                    class="text-decoration-none btn">
+                                    {{ type.name }}
+                                </router-link>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <!-- /.col -->
+                    <!-- /.col -->
 
-                <div class="col-9 row row-cols-3">
-
-                    <div v-for="restaurant in this.state.restaurants" class="col-4 py-1  px-3">
-                        <router-link :to="{ name: 'singleRestaurant', params: { slug: restaurant.slug } }"
-                            class="card bg-transparent border-0 text-decoration-none">
-                            <img src="https://imgs.search.brave.com/Q37xS1P9QR74fgVCUo7CA6Zpn_woGWjzvP9x8e4nUCk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/cmlzdG9yYW50ZXJv/Y2NhLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvZWxlbWVudG9y/L3RodW1icy9yZXN0/YXVyYW50X2Rvd25z/dGFpcnNfcm9vbS1v/ODYxdmd6cjQ0emlh/M25tMm5zdzlpd2N3/MDc2MW83YXlyeTcz/bXFobXMuanBn"
-                                class="img-fluid" alt="...">
-                            <div class="card-body p-0">
-                                <h5 class="card-title m-0"><strong>
-                                        {{ restaurant.name }}
-                                    </strong></h5>
-                                <div class="row">
-                                    <div class="col text-center" v-for="type in restaurant.types">
-                                        <div class="badge bg-danger">
-                                            {{ type.name }}
+                    <div class="col-9">
+                        <div class="row row-cols-3">
+                            <div v-for="restaurant in this.state.restaurants" class="col-4 py-1  px-3">
+                            <router-link :to="{ name: 'singleRestaurant', params: { slug: restaurant.slug } }"
+                                class="restaurant card bg-transparent border-0 text-decoration-none">
+                                <img src="https://imgs.search.brave.com/Q37xS1P9QR74fgVCUo7CA6Zpn_woGWjzvP9x8e4nUCk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/cmlzdG9yYW50ZXJv/Y2NhLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvZWxlbWVudG9y/L3RodW1icy9yZXN0/YXVyYW50X2Rvd25z/dGFpcnNfcm9vbS1v/ODYxdmd6cjQ0emlh/M25tMm5zdzlpd2N3/MDc2MW83YXlyeTcz/bXFobXMuanBn"
+                                    class="card-img-top rounded-3" alt="...">
+                                <div class="card-body px-0 py-3">
+                                    <h5 class="card-title m-0 text-white"><strong>
+                                            {{ restaurant.name }}
+                                        </strong></h5>
+                                    <div class="row">
+                                        <div class="col" v-for="type in restaurant.types">
+                                            <div class="badge bg-danger">
+                                                {{ type.name }}
+                                            </div>
                                         </div>
                                     </div>
+                                    <!-- /.row -->
                                 </div>
-                                <!-- /.row -->
+                            </router-link>
+                            <!-- /.card -->
+
                             </div>
-                        </router-link>
-                        <!-- /.card -->
-
+                        </div>
+                        
                     </div>
-                </div>
-                <!-- /.col -->
+                    <!-- /.col -->
 
-            </div>
+                </div>
             <!-- /.row -->
         </div>
         <!-- /.container -->
+
+        </section>
+        
     </main>
 </template>
 
@@ -72,6 +80,34 @@ export default {
 @use '../assets/scss/partials/variables' as *;
 
 main {
-    background-color: $d_boo_primary;
+    background-image: url(https://w0.peakpx.com/wallpaper/1019/191/HD-wallpaper-restaurant-and-bar-with-gorgeous-view-city-restaurant-view-bar.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+section{
+    background-color: rgba(0, 0, 0, 0.500);
+}
+.type_list{
+    max-height: 600px;
+    overflow-y: scroll;
+}
+.overflow_hidden {
+    overflow-y: scroll;
+}
+.overflow_hidden::-webkit-scrollbar {
+    display: none;
+
+}
+.btn{
+    color: white;
+    font-size: 24px;
+}
+.btn:hover{
+    color: greenyellow;
+}
+.restaurant:hover{
+    width: 320px;
+    height: 200px;
 }
 </style>
--- src/views/SingleRestaurantView.vue
+++ src/views/SingleRestaurantView.vue
@@ -29,41 +29,45 @@ export default {
 
 <template>
     <main>
+        <section>
 
-        <div class="container">
-            <div v-if="singleRestaurant" class="row">
-                <div class="col-3">
-                    <h1>{{ singleRestaurant.name }}</h1>
-                    <div>{{ singleRestaurant.address }}</div>
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-md-9 row row-cols-1  row-cols-md-2 ">
-                    <div v-for="dish in singleRestaurant.dish" class="col col-md-6 py-1  p-3">
-                        <div class="card bg-transparent border-0 text-decoration-none">
-                            <img :src="base_url + 'storage/' + dish.img" class="img-fluid" alt="">
+            <div class="container">
+                <div v-if="singleRestaurant" class="row">
+                    <div class="col-3">
+                        <h1>{{ singleRestaurant.name }}</h1>
+                        <div>{{ singleRestaurant.address }}</div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-12 col-md-9 row row-cols-1  row-cols-md-2 ">
+                        <div v-for="dish in singleRestaurant.dish" class="col col-md-6 py-1  p-3">
+                            <div class="card bg-transparent border-0 text-decoration-none">
+                                <img :src="base_url + 'storage/' + dish.img" class="img-fluid" alt="">
 
-                            <div class="card-body p-0">
-                                <h5 class="card-title m-0"><strong>
-                                        {{ dish.name }}
-                                    </strong></h5>
-                                <div class="row">
-                                    <div class="col text-center">
-                                        <div class="badge bg-danger">
-                                            {{ dish.price }}
+                                <div class="card-body p-0">
+                                    <h5 class="card-title m-0"><strong>
+                                            {{ dish.name }}
+                                        </strong></h5>
+                                    <div class="row">
+                                        <div class="col text-center">
+                                            <div class="badge bg-danger">
+                                                {{ dish.price }}
+                                            </div>
                                         </div>
                                     </div>
+                                    <!-- /.row -->
                                 </div>
-                                <!-- /.row -->
                             </div>
                         </div>
-                    </div>
 
+                    </div>
+                    <!-- /.col -->
                 </div>
-                <!-- /.col -->
+                <!-- /.row -->
             </div>
-            <!-- /.row -->
-        </div>
         <!-- /.container -->
+        
+        </section>
+        
 
     </main>
 </template>
@@ -72,6 +76,12 @@ export default {
 @use '../assets/scss/partials/variables' as *;
 
 main {
-    background-color: $d_boo_primary;
+    background-image: url(https://w0.peakpx.com/wallpaper/1019/191/HD-wallpaper-restaurant-and-bar-with-gorgeous-view-city-restaurant-view-bar.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+section{
+    background-color: rgba(0, 0, 0, 0.500);
 }
 </style>
